4dece7d63d917d0dc69ce357903b95cc2e1efaae
Karim Alibhai <karim@alibhai.co>
2019-04-05 12:08:52 -0400

    Add: complexity to bare

--- benchmarks/bare.js
+++ benchmarks/bare.js
@@ -1,8 +1,34 @@
-'use strict'
+const url = require('url')
+const qs = require('querystring')
 
-const server = require('http').createServer(function (req, res) {
-  res.setHeader('content-type', 'application/json; charset=utf-8')
-  res.end(JSON.stringify({ hello: 'world' }))
-})
-
-server.listen(3000)
+require('http')
+  .createServer((req, res) => {
+    const parsed = url.parse(req.url)
+    const query = qs.parse(parsed.query)
+    if (req.method === 'POST' && parsed.pathname === '/hello') {
+      let buffer = ''
+      req.on('data', chunk => {
+        buffer += chunk
+      })
+      req.on('end', () => {
+        try {
+          const { name } = JSON.parse(buffer)
+          res.writeHead(200, {
+            'Content-Type': 'application/json',
+          })
+          res.end(JSON.stringify({ hello: name, query }))
+        } catch (err) {
+          res.writeHead(500)
+          res.end(JSON.stringify({ error: err.message }))
+        }
+      })
+    } else {
+      res.writeHead(404)
+      res.end()
+    }
+  })
+  .listen(3000, () => {
+    if (process.env.BENCH_START) {
+      process.exit()
+    }
+  })
